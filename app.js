ec70a8090b971d48f254413c9250a4b68663ff61
Joseph-K-B <303josephb@gmail.com>
2021-06-23 16:17:11 -0700

    added three sound functions

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-// import functions
+// cat function
+const catSound = document.getElementById('cat-sound');
+// const playCat = document.getElementById('play-cat');
+// playCat.addEventListener('click', ()=>{
+//     catSound.play();
+// });
+const cat = document.getElementById('cat');
+cat.addEventListener('click', ()=>{
+  catSound.play();
+});
+// dog function
+const dogSound = document.getElementById('dog-sound');
+const dog = document.getElementById('dog');
+dog.addEventListener('click', ()=>{
+dogSound.play();
+});
 
-// reference needed DOM elements
-
+// horse funstion
+const horseSound = document.getElementById('horse-sound');
+const horse = document.getElementById('horse');
+horse.addEventListener('click', ()=> {
+  horseSound.play();
+});
 
 // set event listeners 
   // get user input(s)
