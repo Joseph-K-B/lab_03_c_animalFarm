4d7487a13700f4c562addca16a37d4a919d51802
Joseph-K-B <303josephb@gmail.com>
2021-06-23 17:27:42 -0700

    added multiple animals

--- app.js
+++ app.js
@@ -6,22 +6,39 @@ const catSound = document.getElementById('cat-sound');
 // });
 const cat = document.getElementById('cat');
 cat.addEventListener('click', ()=>{
-  catSound.play();
+    catSound.play();
 });
 // dog function
 const dogSound = document.getElementById('dog-sound');
 const dog = document.getElementById('dog');
 dog.addEventListener('click', ()=>{
-dogSound.play();
+    dogSound.play();
 });
 
-// horse funstion
+// horse function
 const horseSound = document.getElementById('horse-sound');
 const horse = document.getElementById('horse');
 horse.addEventListener('click', ()=> {
-  horseSound.play();
+    horseSound.play();
 });
-
+//donkey function
+const donkeySound = document.getElementById('donkey-sound');
+const donkey = document.getElementById('donkey');
+donkey.addEventListener('click', ()=> {
+    donkeySound.play();
+});
+//lion function
+const lionSound = document.getElementById('lion-sound');
+const lion = document.getElementById('lion');
+lion.addEventListener('click', ()=> {
+    lionSound.play();
+});
+//elephant function
+const elephantSound = document.getElementById('elephant-sound');
+const elephant = document.getElementById('elephant');
+elephant.addEventListener('click', ()=>{
+  elephantSound.play();
+});
 // set event listeners 
   // get user input(s)
   // do any needed work with the value(s)
